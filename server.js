e2098c611278aa92861e1ca2d7ac0d24a0436b83
chimamodesty <chimavic2000@gmail.com>
2023-09-15 05:11:23 +0100

    common

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use('/', require('./routes'));
-const port = 3000;
+
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
