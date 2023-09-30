0dca505890bf05581c8b3b00d57974be3fc58238
chimamodesty <chimavic2000@gmail.com>
2023-09-30 16:51:01 +0100

    crud operations

--- controller/contacts.js
+++ controller/contacts.js
@@ -13,13 +13,51 @@ const getById = async (req, res, next) => {
     const id = req.params.id
 
     const result = await mongodb.getDb().db().collection('contacts').findOne({'_id': new ObjectId(id)})
-    
         res.setHeader('Content-Type', 'application/json')
         res.status(200).json(result)
     
 }
 
+const addContact = async (req, res, next) => {
+    const {firstName, lastName, favoriteColor, birthday} = req.body
+    
+    const data = {
+        firstName,
+        lastName,
+        favoriteColor,
+        birthday
+    }
+
+    await mongodb.getDb().db().collection('contacts').insertOne(data)
+        .then(result => {
+            res.status(201).json({"message": "created successfully",
+            "insertedId": result.insertedId})
+        }).catch(err => res.status(400).send({"error": err.message}))
+}
+
+
+const updateContact = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id)
+    await mongodb.getDb().db().collection('contacts').updateOne(
+        {'_id': userId},
+        {$set: {...req.body}} 
+        )
+        .then(result => 
+            res.status(204).json(result))
+        .catch(err => res.status(400).json({'error': err.message}))
+}
+
+const deleteContact = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id)
+
+    await mongodb.getDb().db().collection('contacts').deleteOne({'_id': userId})
+        .then(result => res.status(200).json(result))
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    addContact,
+    updateContact,
+    deleteContact
 }
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,4 +5,8 @@ const services = require('../controller/contacts');
 router.get('/', services.getAll);
 router.get('/:id', services.getById)
 
+router.post('/', services.addContact)
+router.put('/:id', services.updateContact)
+router.delete('/:id', services.deleteContact)
+
 module.exports = router;
